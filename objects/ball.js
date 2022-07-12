b6dc871238825b14928b9555a576056993f6f47d
tatevzaqaryan98-inomma <tatev.zaqaryan@inomma.com>
2022-07-12 08:04:58 +0400

    first commit

--- objects/ball.js
+++ objects/ball.js
@@ -2,22 +2,23 @@ const BALL_ORIGIN = new Vector2(25, 25);
 const BALL_DIAMETER = 38;
 const BALL_RADIUS = BALL_DIAMETER / 2;
 const HOLE_RADIUS = 46;
-
+const BALL_VELOCITY = 25;
 class Ball {
-    constructor (position, color) {
-        this.position = position;
+  constructor(position, color) {
+    this.position = position;
 
-        // get ball sprite by its color
-        this.sprite = getBallSpriteByColor(color);
-    }
+    // get ball sprite by its color
+    this.sprite = getBallSpriteByColor(color);
+  }
 
-    // update method is called every frame (see detailed description in the dock) 
-    update (delta) {
-        
-    }
+  // update method is called every frame (see detailed description in the dock)
+  update(delta) {}
+  // draws sprite every frame
+  draw() {
+    Canvas.drawImage(this.sprite, this.position, BALL_ORIGIN);
+  }
 
-    // draws sprite every frame
-    draw () {
-        Canvas.drawImage(this.sprite, this.position, BALL_ORIGIN);
-    }
+  ballVelocityUpdate() {
+    this.position.x += BALL_VELOCITY;
+  }
 }
